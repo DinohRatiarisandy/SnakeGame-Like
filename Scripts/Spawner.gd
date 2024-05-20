6c7b8333921f0125b5afc9243fb22a0ef7a38548
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-05-20 21:19:35 +0300

    [add] spawn a new tail and a new food

---
+++ Scripts/Spawner.gd
@@ -0,0 +1,29 @@
+class_name Spawner extends Node2D
+
+signal tail_added(tail: Tail)
+
+@export var bounds: Bounds
+
+@onready var food_to_eat: Node = %FoodToEat
+@onready var snake_parts: Node = %SnakeParts
+
+var food_scene: PackedScene = preload("res://Scenes/food.tscn")
+var tail_scene: PackedScene = preload("res://Scenes/tail.tscn")
+
+func spanw_food() -> void:
+	var spanw_point: Vector2 = Vector2.ZERO
+	spanw_point.x = randf_range(bounds.x_min + Global.GRID_SIZE, bounds.x_max - Global.GRID_SIZE)
+	spanw_point.y = randf_range(bounds.y_min + Global.GRID_SIZE, bounds.y_max - Global.GRID_SIZE)
+	spanw_point.x = floorf(spanw_point.x / Global.GRID_SIZE) * Global.GRID_SIZE
+	spanw_point.y = floorf(spanw_point.y / Global.GRID_SIZE) * Global.GRID_SIZE
+	var food := food_scene.instantiate()
+	food.position = spanw_point
+	food_to_eat.add_child(food)
+
+
+func spanw_tail(pos: Vector2) -> void:
+	var tail: Tail = tail_scene.instantiate() as Tail
+	tail.position = pos
+	snake_parts.add_child(tail)
+	tail_added.emit(tail)
+
