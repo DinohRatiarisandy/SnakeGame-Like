319c0a342f054817ba790429d156a5645ecd9296
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-05-26 14:53:50 +0300

    add game over and pause logic in the game

--- Scripts/Gameplay.gd
+++ Scripts/Gameplay.gd
@@ -9,6 +9,9 @@ class_name Gameplay extends Node2D
 	- Is paused ?
 """
 
+const GAMEOVER_SCN = preload("res://Scenes/game_over.tscn")
+const PAUSE_SCN = preload("res://Scenes/pause_screen.tscn")
+
 @onready var head: Head = $Head as Head
 @onready var bounds: Bounds = $Bounds as Bounds
 @onready var spawner: Spawner = $Spawner as Spawner
@@ -18,6 +21,9 @@ var time_since_last_move := 0.0
 var speed := 4000.0
 var move_dir := Vector2.ZERO
 var snake_parts: Array[SnakePart] = []
+var gameover_menu: Gameover
+var pause_menu: PauseScn
+var score: int = 0
 
 
 func _ready() -> void:
@@ -38,6 +44,8 @@ func _input(event: InputEvent) -> void:
 		move_dir = Vector2.LEFT
 	elif event.is_action_pressed("move_right") and move_dir != Vector2.LEFT:
 		move_dir = Vector2.RIGHT
+	elif event.is_action_pressed("ui_cancel"):
+		pause_game()
 
 
 func _physics_process(delta: float) -> void:
@@ -45,6 +53,17 @@ func _physics_process(delta: float) -> void:
 	if time_since_last_move >= time_between_move:
 		update_snake()
 		time_since_last_move = 0.0
+
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_WM_WINDOW_FOCUS_OUT:
+		pause_game()
+
+
+func pause_game() -> void:
+	if not pause_menu:
+		pause_menu = PAUSE_SCN.instantiate() as PauseScn
+		add_child(pause_menu)
 
 
 func update_snake() -> void:
@@ -63,6 +82,7 @@ func _on_food_eaten() -> void:
 	spawner.call_deferred("spanw_food")
 	spawner.call_deferred("spanw_tail", snake_parts[-1].last_position)
 	speed += 150.0
+	score += 1
 
 
 func _on_tail_added(tail: Tail) -> void:
@@ -76,4 +96,7 @@ func _on_tail_eaten() -> void:
 	"""
 		Is Game over ?
 	"""
-	print("Game Over")
+	if not gameover_menu:
+		gameover_menu = GAMEOVER_SCN.instantiate() as Gameover
+		add_child(gameover_menu)
+		gameover_menu.set_score(score)
