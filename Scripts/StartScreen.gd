2c5eb918fa8a5c1c19282e293220675abfea3b9f
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-05-26 14:53:14 +0300

    the main menu scene

---
+++ Scripts/StartScreen.gd
@@ -0,0 +1,24 @@
+class_name StartScreen extends CanvasLayer
+
+"""
+	This class is responsable of the MainMenu screen.
+"""
+
+const GAMEPLAY_SCN: PackedScene = preload("res://Scenes/gameplay.tscn")
+
+@onready var high_score_label: Label = %HighScoreLabel
+@onready var start_button: Button = %StartButton
+@onready var quit_button: Button = %QuitButton
+
+
+func _ready() -> void:
+	var high_score: int = 0
+	high_score_label.text = "High Score: " + str(high_score)
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_packed(GAMEPLAY_SCN)
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
